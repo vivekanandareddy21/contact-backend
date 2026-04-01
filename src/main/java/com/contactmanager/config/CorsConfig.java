f1f62bdc56d6ba21cd16bde795b5c8a8a08807a6
vivekanandareddy21 <vivekanandareddy2106@gmail.com>
2026-04-01 23:25:38 +0530

    backend commit

--- src/main/java/com/contactmanager/config/CorsConfig.java
+++ src/main/java/com/contactmanager/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://contact-frontend-9izt.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
